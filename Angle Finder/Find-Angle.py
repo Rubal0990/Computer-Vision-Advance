7da6e128bbdeafc0f89d8f0b41a4e945568be3f9
Rubal0990 <rubal00009@gmail.com>
2021-07-13 20:46:56 +0530

    Updated

--- Angle Finder/Find-Angle.py
+++ Angle Finder/Find-Angle.py
@@ -1,24 +1,32 @@
+# Importing Libraries
 import cv2
 import math
 
+# Reading Images
 path = 'test.jpg'
 img = cv2.imread(path)
+
 pointsList = []
+
+# User-defined function of marking circles
 
 
 def mousePoints(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         size = len(pointsList)
         if size != 0 and size % 3 != 0:
-            cv2.line(img, tuple(pointsList[round((size - 1) / 3) * 3]), (x, y), (0, 0, 255), 2)
+            cv2.line(img, tuple(
+                pointsList[round((size - 1) / 3) * 3]), (x, y), (0, 0, 255), 2)
         cv2.circle(img, (x, y), 5, (0, 0, 255), cv2.FILLED)
         pointsList.append([x, y])
 
 
+# User-defined function to obtain center of the circle
 def gradient(pt1, pt2):
     return (pt2[1] - pt1[1]) / (pt2[0] - pt1[0])
 
 
+# User-defined function of calculating angle between circle
 def getAngle(pointsList):
     pt1, pt2, pt3 = pointsList[-3:]
     m1 = gradient(pt1, pt2)
@@ -30,11 +38,12 @@ def getAngle(pointsList):
                 1.5, (0, 0, 255), 2)
 
 
+# Logic Loop
 while True:
-
     if len(pointsList) % 3 == 0 and len(pointsList) != 0:
         getAngle(pointsList)
 
+    # Displaying the output Image
     cv2.imshow('Image', img)
     cv2.setMouseCallback('Image', mousePoints)
     if cv2.waitKey(1) & 0xFF == ord('q'):
